73809401b683472c84c11bf60c3cd14cba5fce32
nanu <nadixpeque@gmail.com>
2024-02-25 15:28:11 -0300

    Intro React terminado

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import Botones from "./Botones"
 
-const studentName = "";
+const studentName = "Nadia";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Bienvenidos a mi app.</h1>
+      <div>
+        <h3>{studentName}</h3>
+      </div>
+      <div>
+        <ul>
+          {techSkills.map((skills, index) => {
+            return <li key={index}>{skills}</li>
+          })}
+        </ul>
+      </div>
+      <div>
+        <Botones alerts={alerts}/>
+      </div>
+    </div>
+
+  );
 }
 
 // Esto lo exportamos para los tests
